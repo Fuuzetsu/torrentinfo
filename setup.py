f091bccd815006a559515a2e1e8799980147101c
Mateusz Kowalczyk <mk440@bath.ac.uk>
2012-11-07 19:46:22 +0000

    Change version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 ##############################################################################
 # T O R R E N T I N F O - Setup file
-# version 1.0.2
+# version 1.1.0
 #
 # Vrai Stacey <vrai@acherondevelopment.com>
 ##############################################################################
@@ -10,10 +10,10 @@
 from distutils.core import setup;
 
 setup ( name         = "torrentinfo",
-		version      = "1.0.2",
+		version      = "1.1.0",
 		description  = "Bittorrent .torrent file parser and summariser",
 		author       = "Vrai Stacey",
 		author_email = "vrai@acherondevelopment.com",
 		url          = "http://vrai.net/project.php?project=torrentinfo",
-		license      = "GNU General Public License (see LICENSE file)",
+		license      = "GNU General Public License v2 (see LICENSE file)",
 		scripts      = [ "torrentinfo" ] );
