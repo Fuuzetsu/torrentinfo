35dd33767f2fd2e3c6d8fb1f7406f3ccdf0bdf6d
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-04-19 23:28:09 +0100

    Update version. Fixes #8.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if os.name is 'posix':
 
 
 setup(name="torrentinfo",
-      version="1.7.0",
+      version="1.8.0",
       description="Bittorrent .torrent file parser and summariser",
       author="Mateusz Kowalczyk",
       author_email="fuuzetsu@fuuzetsu.co.uk",
