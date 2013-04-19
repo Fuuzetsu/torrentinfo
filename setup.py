de9916a7bf831ca699546619458c1508e3ca7767
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-04-19 17:18:14 +0100

    Cross-platform install. Posix systems will install without extension. Fixes #5

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 from distutils.core import setup
+import os
 import os.path
 
+install_file = 'torrentinfo.py'
+if os.name is 'posix':
+    src_path = os.path.abspath('src')
+    file_path = os.path.join(src_path, 'torrentinfo')
+    if not os.path.exists(file_path):
+        os.symlink(os.path.join(src_path, 'torrentinfo.py'), file_path)
+    install_file = 'torrentinfo'
+
+
 setup(name="torrentinfo",
-      version="1.6.0",
+      version="1.6.1",
       description="Bittorrent .torrent file parser and summariser",
       author="Mateusz Kowalczyk",
       author_email="fuuzetsu@fuuzetsu.co.uk",
       url="https://github.com/ShanaTsunTsunLove/torrentinfo",
       license="GNU General Public License v2 (see LICENSE file)",
-      scripts=[os.path.join("src", "torrentinfo.py")])
+      scripts=[os.path.join("src", install_file)])
